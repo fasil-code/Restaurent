8d5970200a0015cabcbaab812e3cd0d41b804caf
fasil-code <77100387+fasil-code@users.noreply.github.com>
2022-02-04 00:39:45 -0800

    Update Footer.jsx

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -10,9 +10,9 @@ const Footer = () => (
  <div className="app__footer-links">
       <div className="app__footer-links_contact">
         <h1 className="app__footer-headtext">Contact Us</h1>
-        <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
-        <p className="p__opensans">+1 212-344-1230</p>
-        <p className="p__opensans">+1 212-555-1230</p>
+        <p className="p__opensans">New Colony 192101 Anantnag J&K</p>
+        <p className="p__opensans">+91 6005534204</p>
+        <p className="p__opensans">+91 9080800000</p>
       </div>
 
       <div className="app__footer-links_logo">
